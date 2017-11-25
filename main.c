c6abda575a4e560c0cd35a06ec73b602a617f395
Slidejiveman <rdnotlaw91@outlook.com>
2017-11-24 22:21:44 -0600

    Added in timers. Now works just like the serial version. Parallelization is all that remains from this point.

--- main.c
+++ main.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <time.h>
+#include <stdlib.h>
 
-#define MAX 7 //The maximum size of the array
+#ifdef __CYGWIN__
+# define CLOCK CLOCK_MONOTONIC
+#else
+# define CLOCK CLOCK_MONOTONIC_RAW
+#endif
+
+#define MAX 10000 //The maximum size of the array
 
 // The array of elements to sort
-int intArray[MAX] = {4, 6, 3, 2, 1, 9, 7};
+int intArray[MAX] = {};
 
+/**
+ * Initializes the array with MAX number of random numbers.
+ */
+void initialize_array() {
+    srand(NULL);
+    for (int i = 0; i < MAX; i++) {
+        intArray[i] = rand();
+    }
+}
 /**
  * This method is used to print a line of characters
  * to make the console output more human readable.
@@ -84,7 +101,7 @@ int partition(int left, int right, int pivot) {
 
 /**
  * Quick sort is a recursive algorithm that breaks the array into two different sections at the partition point.
- * Each of these is sorted.
+ * Each of these is sorted. TODO: Parallelize this call. Then, add timers.
  * @param left - the point on the left side of the array to start with
  * @param right - the point on the right side of the array to start with
  */
@@ -94,9 +111,24 @@ void quick_sort(int left, int right) {
     } else {
         int pivot = intArray[right]; // pivot begins as the last element in the array for this program
         int partitionPoint = partition(left, right, pivot);
-        quick_sort(left, partitionPoint - 1);
-        quick_sort(partitionPoint + 1, right);
+
+        // parallelize this part, I think.
+        //# pragma omp parallel num_threads(8) shared(partitionPoint, left, right)
+        {
+            quick_sort(left, partitionPoint - 1);
+            quick_sort(partitionPoint + 1, right);
+        }
     }
+}
+
+/**
+ * Converts the time spec to milliseconds.
+ *
+ * @param ts - A pointer to a timespec
+ * @return The wall time in milliseconds
+ */
+double ts_to_ms(struct timespec* ts) {
+    return (((double) ts->tv_sec) * 1000.0) + (((double) ts->tv_nsec) / 1000000.0);
 }
 
 /**
@@ -104,12 +136,34 @@ void quick_sort(int left, int right) {
  * @return The status code indicating success or failure.
  */
 int main() {
+    struct timespec start, finish;
+
+    initialize_array();
     printf("Input Array: ");
     display();
     printline(50);
+
+    // Collect the processor start time
+    clock_t  begin = clock();
+    // Collect the wall start time
+    clock_gettime(CLOCK, &start);
+    // perform the calculation to measure
     quick_sort(0, MAX-1); // start the sort out with the first and last indices of the array
+    // Collect the processor end time
+    clock_t end = clock();
+    // Collect the wall end time
+    clock_gettime(CLOCK, &finish);
+    // Calculate the processor time
+    double time_spent = (double) (end - begin) / CLOCKS_PER_SEC;
+    // Calculatae the wall time
+    double start_time = ts_to_ms(&start);
+    double finish_time = ts_to_ms(&finish);
+    double wall_time = finish_time - start_time;
+
     printf("Ouput Array: ");
     display();
     printline(50);
+    printf("\nExecution Time: %f", time_spent);
+    printf("\nWall Clock Time: %f", wall_time);
     return 0;
 }
